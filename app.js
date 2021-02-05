49e439d59182761dec8ad8f52f928ac15bc1c716
tyttseng <tyttseng@gmail.com>
2021-02-05 18:04:55 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,35 @@
+// require express
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require mongoose and connect to mongodb
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// require view engines
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting connection message
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log('Listening')
+})
