81701021df0c3bcec1ead21c9b8899b40e284d90
tyttseng <tyttseng@gmail.com>
2021-02-09 18:40:04 +0800

    refactor: modify home page and filter router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,7 +9,7 @@ const Category = require('../../models/category')
 const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({ extended: true }))
 
-// find catIcon
+// Log catIcon
 const iconList = {}
 Category.find()
   .lean()
@@ -21,53 +21,28 @@ Category.find()
 
 // setting routes
 router.get('/', (req, res) => {
+  const category = req.query.category
+  let filteredList = []
+  let totalAmount = 0
   return Record.find()
     .lean()
-    .then(records => {
-      // console.log(records)
-      let totalAmount = 0
-      records.forEach(item => {
-        totalAmount = totalAmount + item.amount
+    .then((records) => {
+      if (!category || category === 'all') {
+        filteredList.push(...records)
+      } else {
+        filteredList = records.filter(item => item.category === category)
+      }
+    })
+    .then(() => {
+      filteredList.forEach(item => {
+        totalAmount += item.amount
         item.icon = iconList[item.category]
       })
-      res.render('index', { records, totalAmount })
+    })
+    .then(() => {
+      res.render('index', { records: filteredList, totalAmount, category })
     })
     .catch(err => console.log(err))
-})
-
-// filter
-router.post('/', (req, res) => {
-  const { category } = req.body
-  if (category === 'all') {
-    return res.redirect('/')
-  } else {
-    const filteredList = []
-    let totalAmount = 0
-    let catIcon = ''
-    return Record.find()
-      .lean()
-      .then((record) => {
-        // find the category items & count amount
-        record.forEach(item => {
-          if (item.category === category) {
-            filteredList.push(item)
-            totalAmount = totalAmount + item.amount
-          }
-        })
-      }) // find the category icon
-      .then(Category.find({ category: category })
-        .lean()
-        .then((cat) => {
-          catIcon = cat[0].categoryIcon
-        }) // put the icon to filtered item & render
-        .then(() => {
-          filteredList.forEach(item => {
-            item.icon = catIcon
-          })
-          res.render('index', { records: filteredList, totalAmount, category })
-        }))
-      .catch(err => console.log(err))
-  }
 })
 
 // export router
