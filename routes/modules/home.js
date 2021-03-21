1a44a94d7faaab5ea31ac2568761a5eecd8a5112
tyttseng <tyttseng@gmail.com>
2021-03-21 15:43:32 +0800

    add month filter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,6 +5,9 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+// require date-fns
+const format = require('date-fns/format')
+
 // Log categoryIcon
 const iconList = {}
 Category.find()
@@ -17,33 +20,43 @@ Category.find()
   .catch(err => console.log(err))
 
 // setting routes
+// TODO: 重構程式碼
 router.get('/', (req, res) => {
-  const category = req.query.category
-  // const month = req.query.month
+  const { category, month } = req.query
   const userId = req.user._id
-  let filteredList = []
   let totalAmount = 0
   return Record.find({ userId })
     .lean()
     .then((records) => {
+      let filteredList = []
       if (!category || category === 'all') {
-        return filteredList.push(...records)
+        filteredList.push(...records)
+        return filteredList
       } else {
         filteredList = records.filter(item => item.category === category)
         return filteredList
       }
     })
-    // .then((filteredList) => {
-    //   console.log(filteredList)
-    // })
-    .then(() => {
+    .then((filteredList) => {
+      if (!month || month === 'all') {
+        return filteredList
+      } else {
+        return filteredList.filter(item => {
+          const itemDate = format(new Date(item.date), 'MMMM dd yyyy')
+          const itemMonth = itemDate.split(' ')[0]
+          return itemMonth.toLowerCase() === month.toLowerCase()
+        })
+      }
+    })
+    .then((filteredList) => {
       filteredList.forEach(item => {
         totalAmount += item.amount
         item.icon = iconList[item.category]
       })
+      return filteredList
     })
-    .then(() => {
-      res.render('index', { records: filteredList, totalAmount, category })
+    .then((filteredList) => {
+      res.render('index', { records: filteredList, totalAmount, category, month })
     })
     .catch(err => console.log(err))
 })
